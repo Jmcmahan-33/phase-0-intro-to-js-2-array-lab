463717c990564c4fb1d88be26dc731ed3e979579
Jared McMahan <jaredm788@gmail.com>
2022-08-21 08:34:32 -0600

    commit tree 66bfd0bb6c862be9b813c9c305e3ea401ad600aa

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(Charles) {
+    return cats.push(Charles)
+
+}
+
+function destructivelyPrependCat(Bob) {
+    return cats.unshift(Bob)
+}
+
+function destructivelyRemoveLastCat() {
+    return cats.pop()
+}
+
+
+function destructivelyRemoveFirstCat() {
+    return cats.shift()
+}
+
+function appendCat(){
+    const copyOfAppendCat = [...cats, "Broom"]
+    return copyOfAppendCat
+}
+
+function prependCat() {
+    const copyOfPrependCat = ["Arnold", ...cats]
+    return copyOfPrependCat
+}
+
+function removeLastCat() {
+    const copyRemoveLastCat = cats.slice(0,2)
+    return copyRemoveLastCat
+}
+
+function removeFirstCat() {
+    const copyRemoveFirstCat = cats.slice(1)
+    return copyRemoveFirstCat
+}
